c1df08a61e059af23d031bb73672ca11b370cdd0
Dmitry <Dmitry>
2016-10-27 14:32:52 +0300

    fix generator for lab 2

--- java/CADaHDL_LabWork3_TestsGenerators/src/Lab2TestGenerator.java
+++ java/CADaHDL_LabWork3_TestsGenerators/src/Lab2TestGenerator.java
@@ -14,6 +14,7 @@ public class Lab2TestGenerator {
     private String _filepathToLab1OutputSignals = "D:\\Projects\\JetBrains\\IntelliJ_IDEA_Workspace\\Term_9\\CADaHDL_LabWork3_TestsGenerators\\files\\Lab2_outs";
     private int _numberDigits = 12;
     private int _binNumberInDecimalView = 4096;
+    private String [] _counterMemmory = {"0", "0", "0", "0"};
     public void inputSignalsGenerate() throws Exception{
         FileWriter filewriter = new FileWriter(new File(_filepath));
         for(int i=0; i < _binNumberInDecimalView; i++){
@@ -65,6 +66,22 @@ public class Lab2TestGenerator {
         return outputSignals;
     }
 
+    boolean convertSignalValToBool(String signal){
+        if((signal == "Z") || (signal == "0")){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    String convertBooleanToStr(boolean signal){
+        if(signal == true){
+            return "1";
+        }
+        else return "0";
+    }
+
     public String generateOutputSignals(String curInputSignals){
         boolean notOE, ENT, ENP, notSCLR, notSLOAD, clk, notACLR, notALOAD, a, b, c, d, CCO, RCO, Qa, Qb, Qc, Qd;
         boolean [] inputInBinary = convertStrToBooleanArr(curInputSignals);
@@ -81,8 +98,37 @@ public class Lab2TestGenerator {
         c = inputInBinary[10];
         d  = inputInBinary[11];
 
-        //
+        //emulate the behavior of counter
+        // 1) Q outputs disabled
+        if(notOE){
+            //_counterMemmory
+            RCO = (ENT && convertSignalValToBool(_counterMemmory[0]) && convertSignalValToBool(_counterMemmory[1]) &&
+                    convertSignalValToBool(_counterMemmory[2]) && convertSignalValToBool(_counterMemmory[2]) );
+            CCO = (RCO || (!clk) || (ENT || ENP));
+            _counterMemmory[0] = "Z";
+            _counterMemmory[1] = "Z";
+            _counterMemmory[2] = "Z";
+            _counterMemmory[3] = "Z";
 
+            String outputSignalsInStr = convertBooleanToStr(CCO) + convertBooleanToStr(RCO) + _counterMemmory[0] +
+                    _counterMemmory[1] + _counterMemmory[2] + _counterMemmory[3];
+            return outputSignalsInStr;
+        }
+        // 2) Asynchronous clear
+
+        // 3) Asynchronous load
+
+        // 4) Synchronous clear
+
+        // 5) Synchronous load
+
+        // 6) Count
+
+        // 7) Inhibit counting
+
+        // 8) Inhibit counting
+
+        /*
         int digits = inputInBinary.length;
         boolean [] outputSignals = new boolean[digits];
         outputSignals[0] = CCO;
@@ -91,7 +137,8 @@ public class Lab2TestGenerator {
         outputSignals[3] = Qb;
         outputSignals[4] = Qc;
         outputSignals[5] = Qd;
-        String outputSignalsInStr = convertBooleanArrToStr(outputSignals);
+        */
+        String outputSignalsInStr = "UUUUUU";
         return outputSignalsInStr;
     }
 
