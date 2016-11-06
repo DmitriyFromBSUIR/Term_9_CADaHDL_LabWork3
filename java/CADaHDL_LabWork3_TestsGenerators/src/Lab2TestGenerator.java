31fcafa1fa1309e1375230f7f6d0e96b1a4e4315
Dmitry <Dmitry>
2016-11-06 18:19:06 +0300

    Big fix for programm logic

--- java/CADaHDL_LabWork3_TestsGenerators/src/Lab2TestGenerator.java
+++ java/CADaHDL_LabWork3_TestsGenerators/src/Lab2TestGenerator.java
@@ -14,6 +14,7 @@ public class Lab2TestGenerator {
     private String _filepathToLab1OutputSignals = "D:\\Projects\\JetBrains\\IntelliJ_IDEA_Workspace\\Term_9\\CADaHDL_LabWork3_TestsGenerators\\files\\Lab2_outs";
     private int _numberDigits = 12;
     private int _binNumberInDecimalView = 4096;
+    private int _nBitCounter = 4;
     private String [] _counterMemmory = {"0", "0", "0", "0"};
     public void inputSignalsGenerate() throws Exception{
         FileWriter filewriter = new FileWriter(new File(_filepath));
@@ -99,12 +100,13 @@ public class Lab2TestGenerator {
         d  = inputInBinary[11];
 
         //emulate the behavior of counter
+        RCO = (ENT && convertSignalValToBool(_counterMemmory[0]) && convertSignalValToBool(_counterMemmory[1]) &&
+                convertSignalValToBool(_counterMemmory[2]) && convertSignalValToBool(_counterMemmory[2]) );
+        CCO = (RCO || (!clk) || (ENT || ENP));
+
         // 1) Q outputs disabled
         if(notOE){
             //_counterMemmory
-            RCO = (ENT && convertSignalValToBool(_counterMemmory[0]) && convertSignalValToBool(_counterMemmory[1]) &&
-                    convertSignalValToBool(_counterMemmory[2]) && convertSignalValToBool(_counterMemmory[2]) );
-            CCO = (RCO || (!clk) || (ENT || ENP));
             _counterMemmory[0] = "Z";
             _counterMemmory[1] = "Z";
             _counterMemmory[2] = "Z";
@@ -114,19 +116,92 @@ public class Lab2TestGenerator {
                     _counterMemmory[1] + _counterMemmory[2] + _counterMemmory[3];
             return outputSignalsInStr;
         }
+
         // 2) Asynchronous clear
+        if((notOE == false) && (notACLR == false)){
+            _counterMemmory[0] = "0";
+            _counterMemmory[1] = "0";
+            _counterMemmory[2] = "0";
+            _counterMemmory[3] = "0";
+
+            String outputSignalsInStr = convertBooleanToStr(CCO) + convertBooleanToStr(RCO) + _counterMemmory[0] +
+                    _counterMemmory[1] + _counterMemmory[2] + _counterMemmory[3];
+            return outputSignalsInStr;
+        }
 
         // 3) Asynchronous load
+        if((notOE == false) && (notACLR == true) && (notALOAD == false)){
+            _counterMemmory[0] = convertBooleanToStr(inputInBinary[8]);
+            _counterMemmory[1] = convertBooleanToStr(inputInBinary[9]);
+            _counterMemmory[2] = convertBooleanToStr(inputInBinary[10]);
+            _counterMemmory[3] = convertBooleanToStr(inputInBinary[11]);
+
+            String outputSignalsInStr = convertBooleanToStr(CCO) + convertBooleanToStr(RCO) + _counterMemmory[0] +
+                    _counterMemmory[1] + _counterMemmory[2] + _counterMemmory[3];
+            return outputSignalsInStr;
+        }
 
         // 4) Synchronous clear
+        if((notOE == false) && (notACLR == true) && (notALOAD == true) && (notSCLR == false) && clk == true){
+            _counterMemmory[0] = "0";
+            _counterMemmory[1] = "0";
+            _counterMemmory[2] = "0";
+            _counterMemmory[3] = "0";
+
+            String outputSignalsInStr = convertBooleanToStr(CCO) + convertBooleanToStr(RCO) + _counterMemmory[0] +
+                    _counterMemmory[1] + _counterMemmory[2] + _counterMemmory[3];
+            return outputSignalsInStr;
+        }
 
         // 5) Synchronous load
+        if((notOE == false) && (notACLR == true) && (notALOAD == true) && (notSCLR == true) && (notSLOAD == false) && clk == true){
+            _counterMemmory[0] = convertBooleanToStr(inputInBinary[8]);
+            _counterMemmory[1] = convertBooleanToStr(inputInBinary[9]);
+            _counterMemmory[2] = convertBooleanToStr(inputInBinary[10]);
+            _counterMemmory[3] = convertBooleanToStr(inputInBinary[11]);
+
+            String outputSignalsInStr = convertBooleanToStr(CCO) + convertBooleanToStr(RCO) + _counterMemmory[0] +
+                    _counterMemmory[1] + _counterMemmory[2] + _counterMemmory[3];
+            return outputSignalsInStr;
+        }
 
         // 6) Count
+        if((notOE == false) && (notACLR == true) && (notALOAD == true) && (notSCLR == true) && (notSLOAD == true) && (ENT == true) && (ENP == true) && clk == true){
+            String result = "";
+            int counterValue = 0;
+            int one = 1;
+            for(int i=0; i<_nBitCounter; i++) {
+                result += _counterMemmory[i];
+            }
+            if(_counterMemmory[3] == "1"){
+                counterValue += (one << 3);
+            }
+            if(_counterMemmory[2] == "1"){
+                counterValue += (one << 2);
+            }
+            if(_counterMemmory[1] == "1"){
+                counterValue += (one << 1);
+            }
+            if(_counterMemmory[0] == "1"){
+                counterValue += 1;
+            }
+            counterValue += 1;
+            String counterVal = Integer.toString(counterValue);
+            for(int i=0; i<_nBitCounter; i++) {
+                _counterMemmory[i] = counterVal.charAt(i);
+            }
+
+        }
 
         // 7) Inhibit counting
+        if((notOE == false) && (notACLR == true) && (notALOAD == true) && (notSCLR == true) && (notSLOAD == true) && (ENT == false)){
+
+        }
 
         // 8) Inhibit counting
+        if((notOE == false) && (notACLR == true) && (notALOAD == true) && (notSCLR == true) && (notSLOAD == true) && (ENP == false)){
+
+        }
 
         /*
         int digits = inputInBinary.length;
